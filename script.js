3b99a943b528d9c8dbc9828a521597dc62e41434
galkowskimateus92-netizen <galkowskimateus92@gmail.com>
2026-03-04 18:00:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-const perfis = [
-  {
-    nome: "Ana",
-    bio: "Gosto de vôlei e açaí",
-    foto: "https://i.pravatar.cc/300?img=5"
-  },
-  {
-    nome: "Julia",
-    bio: "Amo academia e séries",
-    foto: "https://i.pravatar.cc/300?img=6"
-  },
-  {
-    nome: "Marina",
-    bio: "Fã de música e praia",
-    foto: "https://i.pravatar.cc/300?img=7"
-  }
-];
+const card = document.querySelector(".card");
 
-let indice = 0;
+let startX = 0;
+let currentX = 0;
+let isDragging = false;
 
-function mostrarPerfil() {
-  document.getElementById("nome").innerText = perfis[indice].nome;
-  document.getElementById("bio").innerText = perfis[indice].bio;
-  document.getElementById("foto").src = perfis[indice].foto;
+card.addEventListener("mousedown", startDrag);
+card.addEventListener("touchstart", startDrag);
+
+document.addEventListener("mousemove", drag);
+document.addEventListener("touchmove", drag);
+
+document.addEventListener("mouseup", endDrag);
+document.addEventListener("touchend", endDrag);
+
+function startDrag(e) {
+  isDragging = true;
+  startX = e.type.includes("mouse") ? e.clientX : e.touches[0].clientX;
 }
 
-function curtir() {
-  indice++;
-  if (indice >= perfis.length) {
-    indice = 0;
-  }
-  mostrarPerfil();
+function drag(e) {
+  if (!isDragging) return;
+
+  currentX = e.type.includes("mouse") ? e.clientX : e.touches[0].clientX;
+  let moveX = currentX - startX;
+
+  card.style.transform = translateX(${moveX}px) rotate(${moveX * 0.05}deg);
 }
 
-mostrarPerfil();
+function endDrag() {
+  if (!isDragging) return;
+  isDragging = false;
+
+  let moveX = currentX - startX;
+
+  if (moveX > 100) {
+    alert("❤️ Você curtiu!");
+  } else if (moveX < -100) {
+    alert("❌ Você não curtiu!");
+  }
+
+  card.style.transform = "translateX(0px)";
+}
