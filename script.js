5050bde3337e5b03201031b473d0ca86b9c916ab
galkowskimateus92-netizen <galkowskimateus92@gmail.com>
2026-03-04 18:33:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const card = document.querySelector(".card");
+  const like = document.querySelector(".like");
+  const nope = document.querySelector(".nope");
 
   let startX = 0;
   let moveX = 0;
@@ -16,8 +18,17 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!isDragging) return;
 
     moveX = e.clientX - startX;
+
     card.style.transform =
       "translateX(" + moveX + "px) rotate(" + moveX * 0.05 + "deg)";
+
+    if (moveX > 0) {
+      like.style.opacity = Math.min(moveX / 100, 1);
+      nope.style.opacity = 0;
+    } else {
+      nope.style.opacity = Math.min(Math.abs(moveX) / 100, 1);
+      like.style.opacity = 0;
+    }
   });
 
   document.addEventListener("pointerup", function() {
@@ -26,6 +37,9 @@ document.addEventListener("DOMContentLoaded", function () {
     isDragging = false;
     card.style.transition = "transform 0.3s ease";
     card.style.transform = "translateX(0px)";
+
+    like.style.opacity = 0;
+    nope.style.opacity = 0;
   });
 
 });
