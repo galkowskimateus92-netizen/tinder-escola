0659b84f155f5589dddb7094cd78876b9b140e31
galkowskimateus92-netizen <galkowskimateus92@gmail.com>
2026-03-04 18:13:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   const card = document.querySelector(".card");
-  card.style.background = "blue";
+
+  let startX = 0;
+  let moveX = 0;
+  let isDragging = false;
+
+  card.addEventListener("pointerdown", (e) => {
+    isDragging = true;
+    startX = e.clientX;
+    card.style.transition = "none";
+  });
+
+  document.addEventListener("pointermove", (e) => {
+    if (!isDragging) return;
+
+    moveX = e.clientX - startX;
+    card.style.transform =
+      translateX(${moveX}px) rotate(${moveX * 0.05}deg);
+  });
+
+  document.addEventListener("pointerup", () => {
+    if (!isDragging) return;
+    isDragging = false;
+
+    card.style.transition = "0.3s ease";
+
+    if (moveX > 120) {
+      card.style.transform = "translateX(500px)";
+      setTimeout(() => {
+        alert("❤️ Você curtiu!");
+        resetCard();
+      }, 300);
+    } 
+    else if (moveX < -120) {
+      card.style.transform = "translateX(-500px)";
+      setTimeout(() => {
+        alert("❌ Você não curtiu!");
+        resetCard();
+      }, 300);
+    } 
+    else {
+      resetCard();
+    }
+  });
+
+  function resetCard() {
+    card.style.transform = "translateX(0px)";
+    moveX = 0;
+  }
+
 });
